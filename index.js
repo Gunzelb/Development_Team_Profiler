8c1a825c12d5836dd4db8bf439bf05937c8acd83
Gunzelb <brandongunzel@gmail.com>
2021-08-02 17:56:29 -0700

    debugged index.js

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 //Libraries
 const inquirer = require('inquirer');
 const fs = require('fs');
-const art = require('ascii-art');
+// const art = require('ascii-art');
 
 //Classes
 const Manager = require('./lib/manager');
 const Engineer = require('./lib/engineer');
 const Intern = require('./lib/intern');
 
+//Global Variables
 let employees = [];
 
 const opening = [
   {
     type: 'input',
-    message: art.font('Team Profile Generator', doom, true) + '\nWelcome to the Team Profile Generator. Please enter the name of the team manager:',
+    // art.font('Team Profile Generator', doom, true) +
+    message: '\nWelcome to the Team Profile Generator. Please enter the name of the team manager:',
     name: 'name'
   },
   {
@@ -78,24 +80,25 @@ const internQ = [
   }
 ];
 
+//functions
 function generateHTML() {
-  let html = `<!DOCTYPE html>\n
-  <html lang="en">\n
-  <head>\n
-    <meta charset="UTF-8">\n
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">\n
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">\n
-    <title>Development Team Page</title>\n
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">\n
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.3/css/all.css">\n
-  </head>\n
-  <body>\n
-    <div>\n
-      <header class='bg-danger d-flex justify-content-center'>\n
-        <h1 class='text-white'>Development Team</h1>\n
-      </header>\n
-    </div>\n
-    <div class='justify-content-center row'>\n`;
+  let html = `<!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Development Team Page</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.3/css/all.css">
+  </head>
+  <body>
+    <div>
+      <header class='bg-danger d-flex justify-content-center'>
+        <h1 class='text-white'>Development Team</h1>
+      </header>
+    </div>
+    <div class='justify-content-center d-flex'>`;
     let card = '';
     let cards = '';
   employees.forEach((employee) => {
@@ -109,61 +112,70 @@ function generateHTML() {
       extra = `<li class="list-group-item">School: ${employee.school}</li>\n`
     } else if (employee.getRole() === 'Manager') {
       icon = '<i class="fas fa-mug-hot"></i>'
-      extra = `<li class="list-group-item">Office Number: ${employee.office}</li>\n`
+      extra = `<li class="list-group-item">Office Number: ${employee.officeNumber}</li>\n`
     } else {
       icon = '';
-      extra = '/n';
+      extra = '';
     };
-    card = `<div class="card m-3" style="width: 18rem;">\n
-    <div class="bg-primary card-body">\n
-      <h5 class="card-title">${employee.name}</h5>\n
-      <div>\n
-        ${icon}\n
-        <h6 class="card-subtitle d-inline mb-2">${employee.getRole()}</h6>\n
-      </div>\n
-    </div>\n
-    <ul class="list-group list-group-flush">\n
-      <li class="list-group-item">ID: ${employee.id}</li>\n
-      <li class="list-group-item">Email: ${employee.email}</li>\n
+    card = `<div class="card m-3" style="width: 18rem;">
+    <div class="bg-primary card-body">
+      <h5 class="card-title">${employee.name}</h5>
+      <div>
+        ${icon}
+        <h6 class="card-subtitle d-inline mb-2">${employee.getRole()}</h6>
+      </div>
+    </div>
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item">ID: ${employee.id}</li>
+      <li class="list-group-item">Email: ${employee.email}</li>
       ${extra}
-    </ul>\n`;
-    cards.concat(card);
+    </ul>
+    </div>`;
+    cards = cards.concat(' ', card);
   })
-  let end = `</div>\n
-  </body>\n
+  let end = `</div>
+  </body>
   </html>`
-  fs.writeFileSync('./dist/index.html', html + cards + end);
+  let fullPage = html + cards + end;
+  fs.writeFileSync('./dist/index.html', fullPage);
 }
 
 function newEmployee() {
-  inquirer.prompt(options)
+  let startPrompt = inquirer.createPromptModule();
+  startPrompt(options)
   .then((answer) => {
     if(answer.option === 'Engineer') {
       inquirer.prompt(engineerQ)
       .then((answer) => {
         let engineer = new Engineer(answer.name, answer.id, answer.email, answer.github);
         employees.push(engineer);
+        newEmployee();
       })
     } else if (answer.option === 'Intern') {
       inquirer.prompt(internQ)
       .then((answer) => {
         let intern = new Intern(answer.name, answer.id, answer.email, answer.school);
         employees.push(intern);
+        newEmployee();
       })
     } else {
-      return
+      generateHTML();
+      return 
     }
   })
 }
 
 function start() {
-  inquirer.prompt(opening)
+  let startPrompt = inquirer.createPromptModule();
+  startPrompt(opening)
   .then((answers) => {
     let manager = new Manager(answers.name, answers.id, answers.email, answers.office);
     employees.push(manager);
+    newEmployee();
+  })
+  .catch((err) => {
+    console.log(err);
   })
 }
 
 start();
-newEmployee();
-generateHTML();
